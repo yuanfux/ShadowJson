e7c4c145e81ad89e60146fa561f6cff582b38fce
Yuan Fu <fuyuanx@gmail.com>
2018-02-24 17:18:18 +0800

    commit single path

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = class ShadowJson {
 
+  _dc(t) {
+    return t ? JSON.parse(JSON.stringify(t)) : undefined;
+  }
+
 	_f(obj, path) {
     path = path ? path.split('.') : path;
     for (let i = 0 ; obj && i < path.length ; i++) {
@@ -9,6 +13,7 @@ module.exports = class ShadowJson {
   }
 
   _rw(obj, path, val) {
+    // rewrite only if the value exists
     path = path.split('.');
     const len = path.length;
     for (let i = 0 ; obj && i < len ; i++) {
@@ -16,7 +21,7 @@ module.exports = class ShadowJson {
         obj = obj[path[i]];
       } else {
         if (obj[path[i]] !== undefined) {
-          obj[path[i]] = val;
+          obj[path[i]] = this._dc(val);
           return val;
         }
       }
@@ -26,16 +31,14 @@ module.exports = class ShadowJson {
 
   constructor(obj, paths) {
 		this.o = obj;
+    this.s = {};
     if (paths) {
-      this.s = {};
       for (let i = 0 ; i < paths.length ; i++) {
         const r = this._f(obj, paths[i]);
         if (r !== undefined) {
-          this.s[paths[i]] = JSON.parse(JSON.stringify(r));
+          this.s[paths[i]] = this._dc(r);
         }
       }
-    } else {
-      this.s = JSON.parse(JSON.stringify(obj));
     }
 	}
 
@@ -50,19 +53,10 @@ module.exports = class ShadowJson {
   commit(path) {
     if(path) {
       // commit only one change
-      let ro = this._f(this.o, path);
       let rs = this.s[path];
-      if (ro !== undefined && rs !== undefined) {
-        ro = JSON.parse(JSON.stringify(rs));
-        this.s[path] = undefined;
-      }
-    } else {
-      // commit all changes
-      for (const p of Object.keys(this.s)) {
-        let ro = this._f(this.o, p);
-        if (ro !== undefined) {
-          ro = JSON.parse(JSON.stringify(this.s[p]));
-          this.s[p] = undefined;
+      if (rs !== undefined) {
+        if(this._rw(this.o, path, rs) !== undefined) {
+          delete this.s[path];
         }
       }
     }
--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 var assert = require('assert');
 var ShadowJson = require('./dist/ShadowJson');
 
-
-assert.ok(true);
-console.log(ShadowJson);
 var object = {
 	a: 123,
 	b: 'hello',
@@ -12,22 +9,55 @@ var object = {
 
 const sJson = new ShadowJson(object, ['a']);
 
+console.log('original object and shadow object');
 console.log(sJson.o);
 console.log(sJson.s);
 
+console.log('object.a = 234;');
 object.a = 234;
 console.log(sJson.o);
 console.log(sJson.s);
 
-let shadow_a = sJson.sget('a');
+console.log("sJson.s['a'] = 456;");
 sJson.s['a'] = 456;
 console.log(sJson.o);
 console.log(sJson.s);
 
-sJson.commit();
+console.log("sJson.commit('a');");
+sJson.commit('a');
+console.log(object);
 console.log(sJson.o);
 console.log(sJson.s);
 
-console.log(sJson.sget('a'));
-console.log(sJson.sget('b'));
-console.log(sJson.sget('c'));
+var object2 = {
+	Lee: {
+    name: 'Lee David',
+    age: 20,
+    job: 'Engineer',
+    workplace: {
+      name: 'Startup',
+      Location: 'SF'
+    }
+	},
+  Bill: {
+    name: 'Bill Lewis',
+    age: 33,
+    job: 'talkshow host',
+    workplace: {
+      name: 'Bill Lewis Show',
+      Location: 'LA'
+    }
+  }
+}
+
+const shadowJson = new ShadowJson(object2, ['Bill.workplace.name']);
+console.log('original object and shadow object');
+console.log(shadowJson.o);
+console.log(shadowJson.s);
+
+shadowJson.s['Bill.workplace.name']  = 'Lee Show';
+console.log("shadowJson.s['Bill.workplace.name'] = 'Lee Show';");
+console.log("shadowJson.commit('Bill.workplace.name');");
+shadowJson.commit('Bill.workplace.name');
+console.log(shadowJson.o);
+console.log(shadowJson.s);
